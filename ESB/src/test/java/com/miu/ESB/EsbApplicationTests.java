8bdd60502160c03e69156aa3b2816c7b2b8d297e
hagos <111482114+Kytsync@users.noreply.github.com>
2023-05-12 22:00:19 -0500

    initial Service Oriented Architecture set up

---
+++ ESB/src/test/java/com/miu/ESB/EsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miu.ESB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
